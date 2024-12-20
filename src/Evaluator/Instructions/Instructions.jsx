57797edbf0a4054e9e57ebefa6c3c1f0b79b8871
Sreeni <seenu619@gmail.com>
2024-12-20 22:02:33 +0530

    eval certificate

--- src/Evaluator/Instructions/Instructions.jsx
+++ src/Evaluator/Instructions/Instructions.jsx
@@ -12,13 +12,41 @@ import { getCurrentUser } from '../../helpers/Utils';
 import axios from 'axios';
 import { useReactToPrint } from 'react-to-print';
 import TCertificate from './Certificates';
+import jsPDF from 'jspdf';
+import TeacherCertificate from '../../assets/media/Evaluator.jpg';
 import { Link } from 'react-router-dom';
 
 const Instructions = () => {
+    const pdfRef = useRef(null);
+    const currentUser = getCurrentUser('current_user');
+    
+    const handleCertificateDownload = () => {
+        const content = pdfRef.current;
+        const fullName = currentUser?.data[0]?.full_name;
+        const doc = new jsPDF('l', 'mm', [298, 211]);
+
+        doc.addImage(TeacherCertificate, 'JPEG', 0, 0, 298, 211);
+        doc.setFont('helvetica', 'bold');
+        doc.setFontSize(20);
+        doc.setTextColor(133, 38, 0); 
+
+        const textWidth =
+            (doc.getStringUnitWidth(fullName) * doc.getFontSize()) /
+            doc.internal.scaleFactor;
+        const x = 298 / 2;
+        const y = 110;
+
+        doc.text(fullName, x, y, { align: 'center' });
+
+        // Save the certificate as a PDF
+        const certName = `${fullName.replace(/\s+/g, '_')}.pdf`;
+        doc.save(certName);
+    };
+
     // here we can start the evaluator  journey //
     const history = useHistory();
     const dispatch = useDispatch();
-    const currentUser = getCurrentUser('current_user');
+    const currentUser1 = getCurrentUser('current_user');
 
     const instructiondata = useSelector(
         (state) => state?.evaluator.instructionsData
@@ -27,7 +55,7 @@ const Instructions = () => {
     React.useEffect(() => {
         dispatch(getInstructions());
     }, []);
-    const handleCertificateDownload = () => {
+    const handleCertificateDownload1 = () => {
         handlePrintCertificate();
     };
     const componentRef = useRef();
@@ -56,30 +84,43 @@ const Instructions = () => {
                 >
                     <div
                         style={{
-                            textAlign: 'center'
+                            textAlign: 'left'
                         }}
                     >
-                        <p className="text-center">
-                            🎉 Congratulations! Thank you for successfully
-                            completing the evaluation process.
-                            <br />
+                        <h6 className="font-bold" style={{ color: '#2196F3' }}>
+                            🎉 Congratulations! 🎉
+                        </h6>
+                        <p>
+                            Thank you for successfully completing the evaluation
+                            process.{' '}
+                        </p>
+                        <p>
                             We greatly appreciate your dedication and effort in
-                            contributing to this initiative.
-                            <br />
-                            Your certificate of participation/achievement is now
-                            available for download.
-                            <br />
-                            👉 Download Certificate
-                            {/* <div className="text-right"> */}
-                            <Link
-                                to="#"
-                                // className="btn btn-primary btn-lg"
-                                style={{ margin: '1rem' }}
-                                onClick={handleCertificateDownload}
-                            >
-                                Download Certificate
-                            </Link>
-                            {/* </div> */}
+                            contributing to this initiative.{' '}
+                        </p>
+                        <p>
+                            Your certificate of participation is now available
+                            for download.
+                        </p>
+
+                        {/* <div className="text-right"> */}
+                        <Link
+                            to="#"
+                            className="btn btn-lg text-bold"
+                            style={{
+                                backgroundColor: '#007e33',
+                                color: '#fff',
+                                padding: '1rem',
+                                borderRadius: '20px'
+                            }}
+                            onClick={handleCertificateDownload}
+                        >
+                            👉 DOWNLOAD CERTIFICATE
+                        </Link>
+                        {/* </div> */}
+                        <p>
+                            Thank you for being an integral part of this
+                            journey.
                         </p>
                     </div>
 
